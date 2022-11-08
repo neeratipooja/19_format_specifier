aec7aa75033f45968c2701710508e0af2c12800b
neeratipooja <116306537+neeratipooja@users.noreply.github.com>
2022-11-08 19:54:08 +0530

    Add files via upload

---
+++ format_specifiers.py
@@ -0,0 +1,22 @@
+"""
+DAY:tuesday
+DATE:8th nov 2022
+TOPIC:format specifiers
+AUTHOR:pooja
+"""
+a=5.5
+print(int(a))
+print(str(a))
+print("%d" %a)
+print("%f" %a)
+print("%s" %a) 
+print("%.1f" %a)
+print("%8f" %a)
+print("%11f" %a)
+print("%9f" %a)
+b=2.3+6.6
+print("%.3f" %b)
+print('%.8f' %b)
+print("%9f" %b)
+print(b)
+print('%.1f' %b)
