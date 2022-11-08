1ff6d7bf194ef7206d5b409aef2929a06f45337f
neeratipooja <116306537+neeratipooja@users.noreply.github.com>
2022-11-08 20:14:04 +0530

    Add files via upload

---
+++ type_conversion_and_casting.py
@@ -0,0 +1,14 @@
+"""
+DAY:tuesday
+DATE:8th nov 2022
+TOPIC:type conversion and casting
+AUTHOR:pooja
+"""
+z=8
+print("the value of z is",z)
+print("the value of z is %d"%z)
+print("the value of z is"' '+str(z))
+w='9'
+y=int(w)
+x=float(w)
+print(x,y)
